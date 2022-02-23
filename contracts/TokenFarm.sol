f1c146797034974e7e279811740828852e874592
gourav <gvirus21yt@gmail.com>
2022-02-23 20:44:12 +0530

    converted input duration to days, and added getStakingBalance function

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -7,10 +7,9 @@ import "./StakeToken.sol";
 import "hardhat/console.sol";
 
 contract TokenFarm {
-    string public name = "Token farm";
     address public owner;
     uint256 public totalSupply;
-    
+
     GVToken public gvToken;
     StakeToken public stakeToken;
 
@@ -27,9 +26,7 @@ contract TokenFarm {
 
     mapping(address => StakeInfo) public stakeInfos;
     mapping(address => uint256) public rewards;
-    mapping(address => bool) public hasStaked;
-    mapping(address => bool) public isStaking;
-    mapping(address => uint) public userRewardPerTokenPaid;
+    mapping(address => uint256) public userRewardPerTokenPaid;
 
     constructor(GVToken _gvToken, StakeToken _stakeToken) {
         gvToken = _gvToken;
@@ -37,26 +34,25 @@ contract TokenFarm {
         owner = msg.sender;
     }
 
-    function rewardPerToken() public view returns (uint) {
+    function rewardPerToken() public view returns (uint256) {
         if (totalSupply == 0) {
             return 0;
         }
         return
             rewardPerTokenStored +
-            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / totalSupply);
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) /
+                totalSupply);
     }
 
-
-      function earned(address _account) public view returns (uint) {
+    function earned(address _account) public view returns (uint256) {
         return
             ((stakeInfos[_account].stakeAmount *
                 (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
             rewards[_account];
     }
 
-
     modifier updateReward(address _account, uint256 _duration) {
-        // handles reward for each stake
+        
         rewardPerTokenStored = rewardPerToken();
         lastUpdateTime = block.timestamp;
 
@@ -71,25 +67,25 @@ contract TokenFarm {
     {
         require(_amount > 0, "Amount Can't be Zero");
 
+        uint256 totalDays = _duration * 1 days;
+
         totalSupply += _amount;
         StakeInfo memory stakeInfo = StakeInfo(
             msg.sender,
             block.timestamp,
-            _duration,
+            totalDays,
             _amount
         );
 
         stakeInfos[msg.sender] = stakeInfo;
         stakeToken.transferFrom(msg.sender, address(this), _amount);
-
     }
 
     modifier checkCanUnstake(address _account) {
+        uint256 totalDays = stakeInfos[_account].endAt;
         uint256 timePassed = block.timestamp - stakeInfos[msg.sender].startAt;
-        require(
-            timePassed >= stakeInfos[msg.sender].endAt,
-            "Stake duration is higher"
-        );
+
+        require(timePassed >= totalDays, "You can't unstake before stake duration complete.");
         _;
     }
 
@@ -99,11 +95,14 @@ contract TokenFarm {
         stakeToken.transfer(msg.sender, _amount);
     }
 
-    function getReward() external { // also needs to update reward
+    function getReward() external {
+        // also needs to update reward
         uint256 reward = rewards[msg.sender];
         rewards[msg.sender] = 0;
         gvToken.transfer(msg.sender, reward);
     }
 
-
+    function getStakingBalance() external returns (uint256) {
+        return stakeInfos[msg.sender].stakeAmount;
+    }
 }
